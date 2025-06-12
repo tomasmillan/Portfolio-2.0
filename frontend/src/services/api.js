14252dfb7b8ce371be0ef61045436ffaf473a7bd
Tomas <tomas.millan96@gmail.com>
2025-06-12 14:18:24 +0200

    ready for deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:1337/api"; // Cambiar en producción
+const API_URL = import.meta.env.VITE_STRAPI_BASE_URL || 'http://localhost:1337';; // Cambiar en producción
 
 export const getPosts = async () => {
   try {
