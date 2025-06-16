ecc3483d9ac14504e9e774c336bfc0cb0e2d8205
Tomas <tomas.millan96@gmail.com>
2025-06-16 11:49:45 +0200

    new deploy

--- blog/config/database.ts
+++ blog/config/database.ts
@@ -1,27 +1,19 @@
-// path: ./config/database.ts
-import { parse } from 'pg-connection-string'; 
+import { parse } from 'pg-connection-string';
 
-export default ({ env }) => {
-  const config = parse(env('DATABASE_URL') as string); 
-console.log(config)
-  return {
+const { host, database, user, password, port } = parse(process.env.DATABASE_URL || '');
+
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host,
+      port: Number(port),
+      database,
+      user,
+      password,
       ssl: {
-        rejectUnauthorized: false // Asegúrate de que esto siga aquí
+        rejectUnauthorized: false,
       },
-      // ¡AÑADE ESTE BLOQUE DE 'pool'!
-      pool: {
-        min: 2, // Mínimo de conexiones en el pool
-        max: 10 // Máximo de conexiones en el pool
-      },
-      // Opcional: Si quieres un timeout explícito para adquirir conexiones del pool
-       acquireConnectionTimeout: 60000 // 60 segundos
     },
-  };
-};
+  },
+});
