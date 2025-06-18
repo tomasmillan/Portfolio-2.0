73d3ff6ef9b5af7463f675edb4c5626c7ce02d43
Tomas <tomas.millan96@gmail.com>
2025-06-18 11:18:35 +0200

    some changes

--- blog/config/database.ts
+++ blog/config/database.ts
@@ -1,25 +1,41 @@
-// backend/config/database.ts
-import { parse } from 'pg-connection-string';
+import path from "path";
 
-// Desestructuras las partes de la DATABASE_URL
-const { host, database, user, password, port } = parse(process.env.DATABASE_URL || '');
+export default ({ env }) => {
+  const client = env("DATABASE_CLIENT", "postgres");
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env("DATABASE_URL"),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+  };
 
-export default ({ env }) => ({
-  // Este es el único objeto 'connection' que Strapi espera aquí
-  connection: {
-    client: 'postgres', // Aquí especificas el cliente
-    host: host, // Aquí usas directamente el host parseado
-    port: Number(port), // Aquí usas directamente el puerto parseado
-    database: database,
-    user: user,
-    password: password,
-    ssl: {
-      rejectUnauthorized: false,
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
-    // No olvides tu configuración de pool que añadimos antes, es importante
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
-});
+  };
+};
