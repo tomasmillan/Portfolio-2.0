9d85d9bc42e1f825c8d9d438e94f755f0aaad68c
Tomas <tomas.millan96@gmail.com>
2025-06-21 05:57:26 +0200

    fix: Consolidated and corrected image flattening logic in api.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="blog" element={<Blog />} />
+          {/* <Route path="blog" element={<Blog />} /> */}
           {/* <Route path="podcast" element={<Podcast />} /> */}
           <Route path="portfolio" element={<Portfolio />} />
           {/* <Route path="blog/:slug" element={<PostDetail />} /> */}
