20bde579b57ba2cffe046578760395e1ed54f759
Tomas <tomas.millan96@gmail.com>
2025-07-29 10:31:06 +0200

    SSL Fixes for new DB

--- blog/config/database.ts
+++ blog/config/database.ts
@@ -1,7 +1,5 @@
-import path from "path";
-
 export default ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
+  const client = env("DATABASE_CLIENT", "postgres"); 
   const connections = {
     postgres: {
       connection: {
@@ -11,15 +9,10 @@ export default ({ env }) => {
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
+        ssl: env.bool("DATABASE_SSL", true) && {
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
+            false 
           ),
         },
         schema: env("DATABASE_SCHEMA", "public"),
